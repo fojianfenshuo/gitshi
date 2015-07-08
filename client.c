d5b9d44c4d08eb330aca9b8e08ea865ddb855ea9
fojian <fojian@localhost.localdomain>
2015-07-08 07:35:08 -0700

    client.c server.c commit

---
+++ client.c
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+
+#define MYPORT  43210
+#define BUFFER_SIZE 1024
+
+int main()
+{
+
+    int sock_cli = socket(AF_INET,SOCK_STREAM, 0);
+
+    struct sockaddr_in servaddr;
+    char a;
+    int flag;
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(MYPORT);
+    servaddr.sin_addr.s_addr = inet_addr("10.210.106.48");
+    if (connect(sock_cli, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("connect");
+        exit(1);
+    }
+    while (1)
+    {
+    	sleep(1);
+        flag = send(sock_cli, &a, 1, 0);
+        printf("send:[%d]||data:[%c]\n", a);
+    }
+
+    close(sock_cli);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <linux/types.h>
+#include <linux/videodev2.h>
+#include <setjmp.h>
+#include "/home/fojian/libjpg/jpeg-8c/jpeglib.h"
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+int g_iSockServerFd;
+int g_iSockAppConn;
+#define MYPORT 43210
+void SocketServerInit()
+{
+	g_iSockServerFd = socket(AF_INET,SOCK_STREAM, 0);
+	
+	struct sockaddr_in server_sockaddr;
+	server_sockaddr.sin_family = AF_INET;
+	server_sockaddr.sin_port = htons(MYPORT);
+	server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	printf("serverfd:[%d]\n", g_iSockServerFd);
+	bind_while:
+	if(bind(g_iSockServerFd,(struct sockaddr *)&server_sockaddr,sizeof(server_sockaddr))==-1)
+	{
+		perror("bind");
+		sleep(1);
+		goto bind_while;
+		return;
+	}
+
+	if(listen(g_iSockServerFd,QUEUE) == -1)
+	{
+		perror("listen");
+		return;
+	}	
+	return;
+}
+void socketAccept()
+{
+	struct sockaddr_in client_addr;
+	socklen_t length = sizeof(client_addr);
+	char a = 'a';
+	int flag;
+	g_iSockAppConn = accept(g_iSockServerFd, (struct sockaddr*)&client_addr, &length);
+	while(1)
+	{
+		sleep(1);
+		flag = recv(g_iSockAppConn, &a, 1, 0);
+		printf("recv:[%d]||data:[%c]\n", flag, a);
+	}
+	return;
+}
